d0911945e556e87774154ca642f7176884614cd4
Jimmysb <jbailey@jimmysoftllc.com>
2021-06-08 08:58:04 -0400

    added more items

--- Basic/Conditionals.js
+++ Basic/Conditionals.js
@@ -1,8 +1,12 @@
+// if block---------------------------------
+
 if (condition) {
     //  block of code to be executed if the condition is true
 } else {
     //  block of code to be executed if the condition is false
 }
+
+// switch statement------------------------
 
 switch (expression) {
     case x:
@@ -13,4 +17,15 @@ switch (expression) {
         break;
     default:
     // code block
+}
+
+// terniary operator-------------------------
+
+let myResult = condition ? exprIfTrue : exprIfFalse
+
+function example() {
+    return condition1 ? value1
+        : condition2 ? value2
+            : condition3 ? value3
+                : value4;
 }
