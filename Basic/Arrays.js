1f4bf0367d1ac1bf79eee177091e65545308aeed
Jimmysb <jbailey@jimmysoftllc.com>
2022-02-09 09:04:55 -0500

    Update arrays and ref sheet

--- Basic/Arrays.js
+++ Basic/Arrays.js
@@ -1,26 +1,15 @@
 // this file has common array manipulations in javascript, used for my blog tutorials
 
 //--------------Big O of array methods---------------------------------------------------------
-
-//  Access O(1)
-//  Push/Pop O(1)
-//  Remove or insert at the beginning O(n)
-//  Search O(n)
-//  Shift/Unshift O(n)
-//  Concat O(n)
-//  Slice O(n)
-//  Splice O(n)
-//  Sort O(n)
-//  forEach O(n)
-//  map O(n)
-//  filter O(n)
-//  reduce O(n)
+//  O(1): Access, Push, Pop 
+//  O(n): shift, unshift, search, concat, slice, splice, forEach, map, filter, reduce
 
 
 //--------------initializing arrays with values and accessing values----------------------------
 
 let myArray = ["dude", "person", "how", "are", "you"];
 // console.log(myArray) // Array(5) ["dude", "person", "how", "are", "you"]
+// console.log(myArray[0]) // dude
 
 let myArray2 = [
     ["dude1", "person1"],
@@ -29,21 +18,13 @@ let myArray2 = [
     ["dude4", "person4"]
 ];
 // console.log(myArray2) // Array(4) [Array(2), Array(2), Array(2), Array(2)]
+// console.log(myArray2[0]) // (2) ['dude1', 'person1']
+// console.log(myArray2.length) // 4, the outer array
+// console.log(myArray2[0].length) // 2, the inner array
 
-let myArray3 = new Array(0, 1, 2, 3);  // do not use this use myArray3 = [0, 1, 2, 3] instead
+let myArray3 = new Array(0, 1, 2, 3);  // this is a constructor
 // console.log(myArray3) // Array(4) [0, 1, 2, 3]
-
-let myArrayVal = myArray[0];
-// console.log(myArrayVal) // dude
-
-let myArrayVal2 = myArray2[0][1];
-// console.log(myArrayVal2) //person1
-
-let lengthOuterArray = myArray2.length;
-// console.log(lengthOuterArray) //person1
-
-let lengthInnerArray = myArray2[0].length;
-// console.log(lengthInnerArray) //person1
+// console.log(myArray3[3]) // 3
 
 //-------------------------copying arrays to other arrays------------------------------
 
@@ -52,91 +33,85 @@ let myArray4 = JSON.parse(JSON.stringify(myArray)); //deep copy array changes to
 
 //------------------------- string operations on arrays --------------------------------
 
-let myString = myArray.toString() //converts to a string of command seperated values
+let myString = myArray.toString() //converts to a string of comma separated values
 // console.log(myString) // dude,person,how,are,you
 
-let myString2 = myArray4.join(","); // joins all elements into a sring
+let myString2 = myArray4.join(","); // joins all elements into a string
 // console.log(myString2); // dude,person,how,are,you
 
 //---------------------adding and removing elements in an array-------------------
 
-myArray.pop() //removes last element
-let myValPop = myArray.pop() // returns the popped out element
-// console.log(myValPop) // are
+let myValPop = myArray.pop() // removes the last element and returns the popped out element
+// console.log(myValPop) // you
 
-myArray.shift() //removes first element
-let myValShift = myArray.shift() // returns the shifted out element
-// console.log(myValShift) // person
+let myValShift = myArray.shift() // removes first element and returns the shifted out element
+// console.log(myValShift) // dude
 
-myArray.push('myVal') //adds element to the end
-let myValPush = myArray.push('myPushVal') //returns the count
-// console.log(myValPush) // 3
+let myValPush = myArray.push('pushedVal') //adds element to the end and returns the length
+// console.log(myValPush) // 4
 
-myArray.unshift('myVal') //adds element to the beginnng
-let myValUnshift = myArray.unshift('myUnshiftVal')  //returns the count
+let myValUnshift = myArray.unshift('myUnshiftVal')  //adds element to the beginning and returns the length
 // console.log(myValUnshift) // 5
 
-myArray2[0].push('myVal') //adds element to the end of first inner array
-let myValPushMyArray2 = myArray2[0].push('myPushVal') //returns the count
-// console.log(myValPushMyArray2, myArray2) // 4, Array(4) [Array(4), Array(2), Array(2), Array(2)]
+let myValPushMyArray2 = myArray2[0].push('myPushVal') //adds element to the end of first inner array returns the length
+// console.log(myValPushMyArray2) // 3
 
-//-------------------removing and changing elemets in an array--------------------------------------
+//-------------------removing and changing elements in an array--------------------------------------
 
-myArray[0] = "myFirstElement"
-// console.log(myArray)  //Array(5) ["myFirstElement", "myVal", "how", "myVal", "myPushVal"]
+myArray[0] = "outerElement"
+// console.log(myArray)  // (5) ['outerElement', 'person', 'how', 'are', 'pushedVal']
 
-myArray2[0][1] = "mySecondElementInFirstSubArray"
-// console.log(myArray2[0])  //Array(4) ["dude1", "mySecondElementInFirstSubArray", "myVal", "myPushVal"]
+myArray2[0][1] = "innerElement"
+// console.log(myArray2[0])  // (3) ['dude1', 'innerElement', 'myPushVal']
 
 delete myArray[0] //deletes the first element, there still is a hole in the array and set to undefined.
-// console.log(myArray)  //Array(5) […, "myVal", "how", "myVal", "myPushVal"]
-// console.log(myArray[0])  //undefined
+// console.log(myArray[0])  // undefined
 
 myArray.splice(0, 2, 'Dudette', 'Sally') // starts at index, how many to remove, item list
-// console.log(myArray)  //Array(5) ["Dudette", "Sally", "how", "myVal", "myPushVal"]
+// console.log(myArray)  // (5) ['Dudette', 'Sally', 'how', 'are', 'pushedVal']
 
 myArray.splice(0, 0, 'Dudette', 'Sally') // starts at index, how many to remove, item list
-// console.log(myArray)  //Array(7) ["Dudette", "Sally", "Dudette", "Sally", "how", "myVal", "myPushVal"]
+// console.log(myArray)  // (7) ['Dudette', 'Sally', 'Dudette', 'Sally', 'how', 'are', 'pushedVal']
 
 myArray.splice(0, 4) // starts at index, how many to remove, item list
-//console.log(myArray)  //Array(3) ["how", "myVal", "myPushVal"]
+// console.log(myArray)  // (3) ['how', 'are', 'pushedVal']
 
 //---------------------merging arrays------------------------------------
 
-let myMergeArray = myArray.concat(myArray3) //combines myArray and mArray3 return a new arry note mixed types can be possible
-// console.log(myMergeArray)  //Array(7) ["how", "myVal", "myPushVal", 0, 1, 2, 3]
+let myMergeArray = myArray.concat(myArray3) //combines myArray and mArray3 return a new array note mixed types can be possible
+// console.log(myMergeArray)  // (7) ['how', 'are', 'pushedVal', 0, 1, 2, 3]
 
 let myMergeArray2 = myArray.concat(myArray3, myMergeArray) //can merge more than two arrays
-// console.log(myMergeArray2)  //Array(14) ["how", "myVal", "myPushVal", 0, 1, 2, 3, "how", …]
+// console.log(myMergeArray2)  // (14) ['how', 'are', 'pushedVal', 0, 1, 2, 3, 'how', 'are', 'pushedVal', 0, 1, 2, 3]
 
 let myMergeArray3 = myArray.concat("value1", "value2", myArray3) //can merge mixture of types including values
-// console.log(myMergeArray3)  //["how", "myVal", "myPushVal", "value1", "value2", 0, 1, 2, …]
+// console.log(myMergeArray3)  // (9) ['how', 'are', 'pushedVal', 'value1', 'value2', 0, 1, 2, 3]
 
 //---------------------slicing arrays------------------------------------
 
 let mySliceArray = myArray.slice(0, 2) // copies a section of an array to another array starting at index and how many elements
-// console.log(mySliceArray)  // Array(3) ["how", "myVal"]
+// console.log(mySliceArray)(2)['how', 'are']
 
 let mySliceArray2 = myArray.slice(0) // copies a section of an array to another array starting at index and if second parameter missing copies to the end
-// console.log(mySliceArray2)  // Array(3) ["how", "myVal", "myPushVal"]
+// console.log(mySliceArray2)  // (3) ['how', 'are', 'pushedVal']
 
 //---------------------sorting arrays------------------------------------
 
 let myArraySort = ["a", "c", "d", "f", "b", 1, 4, 2, 3];
 
-myArraySort.sort() // will sort lowest to highest array alphabetically, not numbers are interpreted as strings
-// console.log(myArraySort)  // Array(9) [1, 2, 3, 4, "a", "b", "c", "d", …]
+myArraySort.sort() // will sort lowest to highest array alphabetically, numbers are interpreted as strings
+// console.log(myArraySort)  // (9) [1, 2, 3, 4, 'a', 'b', 'c', 'd', 'f']
 
-myArraySort.reverse() // will sort highest to lowest array alphabetically, not numbers are interpreted as strings
-// console.log(myArraySort)  // Array(9) [1, 2, 3, 4, "a", "b", "c", "d", …]
+myArraySort.reverse() // will sort highest to lowest array alphabetically, numbers are interpreted as strings
+// console.log(myArraySort)  // (9) ['f', 'd', 'c', 'b', 'a', 4, 3, 2, 1]
 
 let myArraySortNumbers = [10, 20, 1, 5, 9, 3, "a", "b"];
 
-myArraySortNumbers.sort(function (a, b) { return a - b }) // will sort highest to lowest numerically
-// console.log(myArraySortNumbers)  // Array(8) [1, 3, 5, 9, 10, 20, "a", "b"]  note 
+myArraySortNumbers.sort((a, b) => { return a - b }) // will sort highest to lowest numerically
+// console.log(myArraySortNumbers)  // (8) [1, 3, 5, 9, 10, 20, 'a', 'b']
 
-myArraySortNumbers.reverse(function (a, b) { return a - b }) // will sort lowest to highest numerically
-// console.log(myArraySortNumbers)  // Array(8) ["b", "a", 20, 10, 9, 5, 3, 1]
+myArraySortNumbers.reverse((a, b) => { return a - b }) // will sort lowest to highest numerically
+// console.log(myArraySortNumbers)  // (8) ['b', 'a', 20, 10, 9, 5, 3, 1]
 
 //---------------------finding min an max in an array------------------------------------
 // Note you can use sort to find the MIN or MAX value by using the compare operation accessing the 
@@ -145,10 +120,10 @@ myArraySortNumbers.reverse(function (a, b) { return a - b }) // will sort lowest
 let myArrayOfNumber = [1, 3, 26, 6, 7, 90, -1, -30, 5]
 
 let myMinNumber = Math.min.apply(null, myArrayOfNumber)
-// console.log(myMinNumber) //-30
+// console.log(myMinNumber) // -30
 
 let myMaxNumber = Math.max.apply(null, myArrayOfNumber)
-// console.log(myMaxNumber) //90
+// console.log(myMaxNumber) // 90
 
 function myArrayMin(arr) {
     let myIndex = arr.length - 1;
@@ -161,7 +136,7 @@ function myArrayMin(arr) {
     }
     return min;
 }
-// console.log(myArrayMin(myArrayOfNumber))
+// console.log(myArrayMin(myArrayOfNumber)) // -30
 
 function myArrayMax(arr) {
     let myIndex = arr.length - 1;
@@ -174,7 +149,7 @@ function myArrayMax(arr) {
     }
     return max;
 }
-// console.log(myArrayMax(myArrayOfNumber))
+// console.log(myArrayMax(myArrayOfNumber)) // 90
 
 // ---------------------------sorting object arrays-------------------------------------------
 
@@ -184,7 +159,7 @@ let cars = [
     { type: "BMW", year: 2010 }
 ];
 
-cars.sort(function (a, b) { return a.year - b.year });
+cars.sort((a, b) => { return a.year - b.year });
 // console.log(cars[0]); //Object {type: "Volvo", year: 2016}
 // console.log(cars[1]); //Object {type: "BMW", year: 2010}
 // console.log(cars[2]); //Object {type: "Volvo", year: 2016}
@@ -230,7 +205,7 @@ function filterFunction(value, index, array) {
 // console.log(over18);  //Array(2) [45, 25]
 
 //------------------------------reduce----------------------------------------------------------
-// runs a function on each array element to produce (reduce it to) a single value
+// runs a function on each array element to reduce it to a single value
 // left-to-right in the array
 // The total (the initial value or the previously returned value)
 // The item value
@@ -239,7 +214,7 @@ function filterFunction(value, index, array) {
 let numbersReduce = [45, 4, 9, 16, 25];
 let sum = numbersReduce.reduce(reduceFunction);
 
-function reduceFunction(total, value, index, array) {
+function reduceFunction(total, value) {
     // console.log(total, value)
     // 45 4
     // 49 9
@@ -247,7 +222,7 @@ function reduceFunction(total, value, index, array) {
     // 74 25
     return total + value;
 }
-// console.log(sum) //99
+// console.log(sum) // 99
 
 //------------------------------searching arrays----------------------------------------------------------
 
@@ -257,7 +232,7 @@ function findItem(item, numbersToSearch) {
     return numbersToSearch.indexOf(item)
 };
 
-console.log(findItem(16, numbersToSearch)); // 3
+//console.log(findItem(16, numbersToSearch)); // 3
 
 ///------------------------------reduce-----------------------------------------------
 
